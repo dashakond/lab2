30d5d312fb79cc1c54bf77c3bead1a8e5c7a07b8
daryna <dasha.kondratez@gmail.com>
2024-10-21 13:02:28 +0300

    calculate sum of first 3 elemets

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,6 +1,10 @@
 #include "FuncA.h"
+#include <cmath>
 
 double FuncA::calculate(int n) {
-   
-    return 0.0;
+    double sum = 0.0;
+    for (int i = 0; i < 3; ++i) {
+        sum += pow(-1, i) * pow(1, 2 * i) / tgamma(2 * i + 1); // tgamma - гамма-функція
+    }
+    return sum;
 }
